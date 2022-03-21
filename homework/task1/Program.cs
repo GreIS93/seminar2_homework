b06efd19817bb5c1a53833be5b35068cfebb6fa2
Ilya Grekov <grekovis93@gmail.com>
2022-03-21 22:50:24 +0700

    task5

--- homework/task1/Program.cs
+++ homework/task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать числа от -N до N
+Console.WriteLine("Введите число n");
+int n = int.Parse(Console.ReadLine());
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        array[index] = new Random().Next(-n, n);
+        index++;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(position);
+        position++;
+    }
+}
